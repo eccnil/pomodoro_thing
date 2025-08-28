498e67863a4c3f4033dfb58bac19a62a06463888
eccnil <eccnil@gmail.com>
2025-08-29 01:28:51 +0200

    ready to test it

--- codigo/src/main.cpp
+++ codigo/src/main.cpp
@@ -1,5 +1,5 @@
 #include "button.hpp"
-#include "pomodoro.cpp"
+#include "pomodoro.hpp"
 #include <Arduino.h>
 #include <avr/io.h>
 #include <button.hpp>
@@ -17,9 +17,15 @@
 #define BUTTON_B 3 // PA7 Physical 6
 
 // global variables
+enum Strategy { counter_strategy, pomodoro_strategy } strategy;
+
 int num = 0; // number to be displayed
 void increment_num() { num++; }
 void decrement_num() { num--; }
+
+Pomodoro pomodoro = Pomodoro();
+void pomodoro_a() { pomodoro.buttonA(); }
+void pomodoro_b() { pomodoro.buttonB(); }
 
 // shiftRegiseter that drives the display inputs
 shiftRegisterPins srPins = {
@@ -38,27 +44,61 @@ Button button_b = Button(BUTTON_B);
 #define DEVICE_QUANTITY 3
 Device *devices[DEVICE_QUANTITY] = {&display, &button_a, &button_b};
 
+int dot_counter_display(Button &a, Button &b) {
+  int dot = 0;
+  if (a.is_pressed())
+    dot = 4;
+  if (b.is_pressed())
+    dot = 1;
+  if (a.is_pressed() && b.is_pressed())
+    dot = 2;
+  return dot;
+}
+
+void display_pomodoro(Pomodoro &p, Segments &d) {
+  Pomodoro::PomodoroDisplay pd = p.display();
+
+  if (pd.num > 9999 || pd.num < 0) {
+    d.show_err();
+  } else {
+    d.set_digit_number(pd.num % 10, pd.dots & 0b0001, 4);
+    d.set_digit_number((pd.num / 10) % 10, pd.dots & 0b0010, 3);
+    d.set_digit_number((pd.num / 100) % 10, pd.dots & 0b0100, 2);
+    d.set_digit_number((pd.num / 1000) % 10, pd.dots & 0b1000, 1);
+  }
+}
+
 void setup() {
   for (int i = 0; i < DEVICE_QUANTITY; i++) {
     devices[i]->init();
   }
 
-  button_a.set_on_tap(increment_num);
-  button_b.set_on_tap(decrement_num);
+  strategy = button_b.is_pressed() ? counter_strategy : pomodoro_strategy;
+
+  switch (strategy) {
+  case counter_strategy:
+    button_a.set_on_tap(increment_num);
+    button_b.set_on_tap(decrement_num);
+    break;
+  case pomodoro_strategy:
+    button_a.set_on_tap(pomodoro_a);
+    button_b.set_on_tap(pomodoro_b);
+    break;
+  }
 }
 
 void loop() {
   for (int i = 0; i < DEVICE_QUANTITY; i++) {
     devices[i]->poll();
+  };
+
+  switch (strategy) {
+  case Strategy::counter_strategy:
+    display.set_number(num, dot_counter_display(button_a, button_b));
+    break;
+  case Strategy::pomodoro_strategy:
+    pomodoro.poll(millis());
+    display_pomodoro(pomodoro, display);
+    break;
   }
-
-  int dot = 0;
-  if (button_a.is_pressed())
-    dot = 4;
-  if (button_b.is_pressed())
-    dot = 1;
-  if (button_a.is_pressed() && button_b.is_pressed())
-    dot = 2;
-
-  display.set_number(num, dot);
 }
--- codigo/src/pomodoro.cpp
+++ codigo/src/pomodoro.cpp
@@ -1,47 +1,4 @@
-class Pomodoro {
-public:
-  struct PomodoroDisplay {
-    int num;
-    char dots;
-  };
-  enum Status { IDLE, RUNNING, PAUSED, SET_D, SET_U, RING };
-
-private:
-  long time_duration = 25 * 60 * 1000, time_counting_to = 0, current_time = 0;
-  long time_left();
-  Status activity_status;
-
-  int time_left_to_display(long t);
-  void extend_time_counting_to(long diff) { time_counting_to += diff; }
-
-public:
-  Pomodoro();
-  void buttonA();
-  void buttonB();
-  void poll(long milis);
-  void start() {
-    activity_status = RUNNING;
-    time_counting_to = current_time + time_duration;
-  }
-  void pause() { activity_status = PAUSED; }
-  void resume() { activity_status = RUNNING; }
-  void setup_dec_mins() {
-    activity_status = SET_D;
-    time_duration = 60 * 1000;
-  }
-  void setup_inc_dec_mins();
-  void setup_unit_mins() { activity_status = SET_U; }
-  void setup_inc_unit_mins();
-  void exit_setup() { activity_status = IDLE; }
-  void reset() {
-    activity_status = IDLE;
-    time_counting_to = 0;
-  }
-  void ring() { activity_status = RING; }
-  PomodoroDisplay display();
-  Status status() { return activity_status; }
-};
-
+#include <pomodoro.hpp>
 Pomodoro::Pomodoro() : activity_status(IDLE) {}
 
 Pomodoro::PomodoroDisplay Pomodoro::display() {
